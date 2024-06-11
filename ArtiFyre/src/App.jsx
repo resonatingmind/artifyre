61e14faf73fb0a2a66b064ad27cde12b6a523486
resonatingmind <adityakumarsince2004@kgpian.iitkgp.ac.inclear>
2024-06-11 21:26:23 +0530

    Adding the files

---
+++ ArtiFyre/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <img src="/artifyre_logo.png"></img>
+        <h1>Coming Soon...</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
